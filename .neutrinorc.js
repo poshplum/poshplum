01e6c2bea618173139e85b7e2275bc2db12ea85e
Sansato <sansato@inator.guru>
2019-01-10 12:20:37 -0800

    Style loader

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -9,21 +9,48 @@ const babelWithDecorators = (neutrino) => { return neutrino.config.module.
   } )
 };
 
+const styles = ['@neutrinojs/style-loader',{
+  test: /\.(css|sass|scss)$/,
+  moduleTest: /\.module\.(css|sass|scss)$/,
+  hot: true,
+  loaders: [
+    // Define loaders as objects. Note: loaders must be specified in reverse order.
+    // ie: for the loaders below the actual execution order would be:
+    // input file -> sass-loader -> postcss-loader -> css-loader -> style-loader/mini-css-extract-plugin
+    {
+      loader: 'postcss-loader',
+      options: {
+        plugins: [require('autoprefixer')]
+      }
+    },
+    {
+      loader: 'sass-loader',
+      useId: 'sass',
+      options: {
+        includePaths: ['absolute/path/a', 'absolute/path/b']
+      }
+    }
+  ]
+}];
+
 module.exports = {
   env: {
     NODE_ENV: {
       // Require minify middleware during production
       development: { use: [
         '@neutrinojs/react-components',
+        styles,
         babelWithDecorators
       ] },
       production: { use: [
           '@neutrinojs/react-components',
+          styles,
           babelWithDecorators
       ] },
       test: { use: [
           '@neutrinojs/node',
           '@neutrinojs/react-components',
+          styles,
           babelWithDecorators,
           ['@neutrinojs/jest', {
             // setup script for the framework
