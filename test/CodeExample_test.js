fa44b9fc8cad4bdf6db0f71fe8433b6abd5583a8
Sansato <sansato@inator.guru>
2019-01-12 10:42:59 -0800

    CodeExample: test plan

---
+++ test/CodeExample_test.js
@@ -0,0 +1,13 @@
+import Layout from '../src/components/layout';
+import React, {Component} from 'react';
+import renderer from 'react-test-renderer';
+import {shallow,mount,render} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import {getClassName} from "../src/helpers/ClassNames";
+
+
+describe('CodeExample', () => {
+  it("labels the example for ARIA")
+  it("labels the code language (jsx by default)");
+  it("shows the <code>");
+})
