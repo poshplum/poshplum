c56e63beea38c8559a4650b5b4a46f714af56eed
Sansato <sansato@inator.guru>
2019-01-20 17:36:23 -0800

    Panel: include <Icon> slot

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -16,6 +16,8 @@ export default class Panel extends Layout {
 
   static slots = {Title, Icon, Header, Body, Footer};
 
+
+
   // static Title = Title;
   // static Icon = Icon;
   // static Header = Header;
@@ -26,13 +28,13 @@ export default class Panel extends Layout {
     let {className="", ...otherProps} = this.props;
     let {Title, Icon, Header, Body, Footer} = this.slots;
 
-    return <div className={`panel ${className}`}>
-      {Title && <div className="panel-title">{Title}</div>}
+    return <div className={`panel ${className}`} {...otherProps}>
+      {Title && <div className="panel-title">{Icon} {Title}</div>}
+      {Header && <div className="panel-header">{Header}</div>}
       <div className="panel-body">
-
-      {Body}
-
+        {Body}
       </div>
+
       {Footer && <div className="panel-footer">{Footer}</div>}
     </div>
   }
