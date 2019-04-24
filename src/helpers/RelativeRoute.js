507aac37fd6fc663c621022cd9c4b90052a47d25
Sansato <sansato@inator.guru>
2019-04-24 11:46:49 -0700

    removed unused files

--- src/helpers/RelativeRoute.js
+++
@@ -1,9 +0,0 @@
-import {Route} from "react-router-dom";
-
-export default class RelativeRoute extends Route {
-  computeMatch(ref,router) {
-    const result = super.computeMatch(ref,router);
-
-    return result
-  }
-}
