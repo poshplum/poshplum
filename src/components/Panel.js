0e61b27aa02434125580be9bd360a1e0bc638db9
Sansato <sansato@inator.guru>
2019-06-26 12:54:29 -0700

    Add aria attributes for Panel

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,33 +2,58 @@ import React from 'react';
 import Layout from "./layout";
 
 const Title = Layout.namedSlot("Title")
-const Icon = Layout.namedSlot("Icon").withMarkup(({className="", children, ...moreProps }) => <span className={`小 ${className}`} {...moreProps}>{children}</span>)
-const Body = Layout.defaultSlot("Body")
+const Icon = Layout.namedSlot("Icon").withMarkup(
+  ({className="", "aria-label": ariaLabel, children, ...moreProps }) => {
+      let ariaAttrs = {"aria-label": ariaLabel}
+      if (!ariaLabel) ariaAttrs = {"aria-hidden": "true"}
+
+      return <span {...ariaAttrs}
+          className={`小簡 ${className}`}
+        {...moreProps}>{children}</span>;
+  }
+);
+const HeaderRight = Layout.namedSlot("HeaderRight").withMarkup(
+  ({className="", children, ...moreProps }) =>
+    <div className={`float-right ${className}`} {...moreProps}>{children}</div>
+);
+
+    const Body = Layout.defaultSlot("Body")
 const Footer = Layout.namedSlot("Footer")
 
 export default class Panel extends Layout {
   static Title = Title;
   static Icon = Icon;
+  static HeaderRight = HeaderRight;
   static Body = Body;
   static Footer = Footer;
 
-  static slots = {Title, Icon, Body, Footer};
+  static slots = {Title, Icon, HeaderRight, Body, Footer};
 
+  componentDidMount() {
+    let input = this.node.current.querySelector('input[type="text"]');
+    if (input) input.focus();
 
+    setTimeout(() => {
+      this.setState({announceTitle: true})
+    }, 1)
 
-  // static Title = Title;
-  // static Icon = Icon;
-  // static Header = Header;
-  // static Body = Body;
-  // static Footer = Footer;
-
+  }
   render() {
     let {className="", ...otherProps} = this.props;
-    let {Title, Icon, Body, Footer} = this.slots;
+    let {Title, Icon, HeaderRight, Body, Footer} = this.slots;
+    let {announceTitle} = this.state || {}
+    if (!this.node) this.node = React.createRef();
 
-    return <div role="main" className={`屋根裏 panel ${className}`} {...otherProps}>
-      {(Icon || Title) && <div className="panel-header">
-        <div className="敬語 panel-title">{Icon}<h2>{Title || "No Panel.Title"}</h2></div>
+    return <div ref={this.node} role="main" className={`屋根裏 panel ${className}`} {...otherProps}>
+      {(Icon || Title || HeaderRight) && <div className="頭 panel-header">
+        <div className="敬語 panel-title">{Icon}
+          {HeaderRight}
+          <h2 aria-live="assertive" aria-atomic="true">
+            <span className="screader">Overlay Panel: Escape to close</span>
+            {announceTitle && <span>&nbsp;</span>}
+            {Title || "No Panel.Title"}
+          </h2>
+        </div>
       </div>}
       <div className="panel-body">
         {Body}
