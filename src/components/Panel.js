a83b45fc7e0ca39c192ef6050b1583bd0808e7b4
Sansato <sansato@inator.guru>
2019-11-07 16:13:42 -0800

    Panel: add slots
     * Panel.Footer can have className and other props
     * Panel.FixedHeader goes below the title and doesn't scroll

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -18,16 +18,27 @@ const HeaderRight = Layout.namedSlot("HeaderRight").withMarkup(
 );
 
     const Body = Layout.defaultSlot("Body")
-const Footer = Layout.namedSlot("Footer")
+const Footer = Layout.namedSlot("Footer").withMarkup(({className="", children, ...props}) => {
+  return <div className={`panel-footer ${className}`} {...props}>{children}</div>
+
+})
+
+const FixedHeader = Layout.namedSlot("FixedHeader").withMarkup(({children, ...props}) => {
+  const err = Object.keys(props).length ? <div className="toast toast-error">FixedHeader doesn't accept extra props.</div> : "";
+  return <>{err}{children}</>
+
+})
+
 
 export default class Panel extends Layout {
   static Title = Title;
   static Icon = Icon;
   static HeaderRight = HeaderRight;
   static Body = Body;
+  static FixedHeader = FixedHeader;
   static Footer = Footer;
 
-  static slots = {Title, Icon, HeaderRight, Body, Footer};
+  static slots = {Title, Icon, HeaderRight, FixedHeader, Body, Footer};
 
   componentDidMount() {
     let input = this.node.current.querySelector('input[type="text"]:not([readonly])');
@@ -40,13 +51,13 @@ export default class Panel extends Layout {
   }
   render() {
     let {className="", class:bareClass, ...otherProps} = this.props;
-    let {Title, Icon, HeaderRight, Body, Footer} = this.slots;
+    let {Title, Icon, HeaderRight, Body, Footer, FixedHeader} = this.slots;
     let {announceTitle} = this.state || {}
     if (!this.node) this.node = React.createRef();
     if (bareClass) throw new Error("use className=, not class=, for Panel class")
 
     return <div ref={this.node} role="main" className={`屋根裏 panel ${className}`} {...otherProps}>
-      {(Icon || Title || HeaderRight) && <div className="頭 panel-header">
+      {(FixedHeader || Icon || Title || HeaderRight) && <div className="頭 panel-header">
         <div className="敬語 panel-title">{Icon}
           {HeaderRight}
           <h2 aria-live="assertive" aria-atomic="true">
@@ -55,12 +66,13 @@ export default class Panel extends Layout {
             {Title || "No Panel.Title"}
           </h2>
         </div>
+        {FixedHeader}
       </div>}
       <div className="panel-body">
         {Body}
       </div>
 
-      {Footer && <div className="panel-footer">{Footer}</div>}
+      {Footer}
     </div>
   }
 }
