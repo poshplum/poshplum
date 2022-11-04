95365a8dd01936b62f437194e34a0ec91493b73f
Sansato <sansato@inator.guru>
2022-11-03 21:34:06 -0700

    experimental: add wmr

---
+++ public/pages/home/index.js
@@ -0,0 +1,34 @@
+import styles from "./style.module.css";
+import { useState } from "preact/hooks";
+import Panel from "src/components/Panel";
+
+export default function Home() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <section class={styles.home}>
+                <h1>Home</h1>
+                <p>This is the home page.</p>
+                <>
+                    <button
+                        style={{ width: 30 }}
+                        onClick={() => setCount(count - 1)}
+                    >
+                        -
+                    </button>
+                    <output style={{ padding: 10 }}>Count: {count}</output>
+                    <button
+                        style={{ width: 30 }}
+                        onClick={() => setCount(count + 1)}
+                    >
+                        +
+                    </button>
+                </>
+            </section>
+            <Panel>
+                <Panel.Header>Hi from Panel</Panel.Header>A panel body
+            </Panel>
+        </>
+    );
+}
---
+++ wmr.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'wmr';
+import postcss from 'rollup-plugin-postcss'
+
+// Full list of options: https://wmr.dev/docs/configuration
+export default defineConfig({
+	/* Your configuration here */
+	alias: {
+		react: 'preact/compat',
+        'react-dom': 'preact/compat',
+        'src/*': 'src'
+    },
+    plugins: [, 
+        postcss({
+            preprocessor: (content, id) => new Promise((resolve, reject) => {
+              const result = sass.renderSync({ file: id })
+              resolve({ code: result.css.toString() })
+            }),
+            sourceMap: true,
+            extensions: ['.sass','.css'],
+            extract: "plum.css",
+          }),
+    ]
+});
